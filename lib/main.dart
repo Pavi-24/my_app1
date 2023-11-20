3c320ed06c770e86c2f3eca572e715aa6d4d52e8
Pavi-24 <pavisentil@gmail.com>
2023-11-20 22:12:30 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Student Details"),
+          backgroundColor: Colors.cyan,
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}): super(key: key);
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const[
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'type something',
+                border: OutlineInputBorder()
+              ),
+            )
+          ],
+        ),
+      ),
+      );
+  }
+}
