525f95a40d66a2fc447a267b7d5c9fddcc395785
Pavi-24 <pavisentil@gmail.com>
2023-11-21 10:06:03 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,12 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Student Details"),
-          backgroundColor: Colors.cyan,
-        ),
-      ),
+      home:MyHomePage(),
     );
   }
 }
@@ -34,22 +29,75 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController _namecontroller=TextEditingController();
+    TextEditingController _regcontroller=TextEditingController();
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Student Details"),
+        backgroundColor: Colors.cyan,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const[
+          children: [
             TextField(
+              controller: _namecontroller,
               decoration: InputDecoration(
-                hintText: 'type something',
+                hintText: 'Type your name',
                 border: OutlineInputBorder()
               ),
-            )
+            ),
+            TextField(
+              controller: _regcontroller,
+              decoration:InputDecoration(
+                hintText: 'Register number',
+                border: OutlineInputBorder()
+              ) ,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>NextPage(
+
+                        ),
+                    ),
+                );
+              },
+              child:Text("Submit"),
+            ),
           ],
         ),
       ),
       );
+  }
+}
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+   final String name;
+   final String reg;
+
+  NextPage({required this.nname});
+  NextPage({required this.reg});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Details")),
+        body: Center(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Text from Textfield: $name'),
+              Text('Text from Textfield: $reg')
+            ],
+
+          ),
+        ),
+    );
   }
 }
